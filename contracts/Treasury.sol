d43e8b924cf361f4318684b3417e0b972be09407
ucash_official <ucash_official@protonmail.com>
2020-12-25 22:58:05 +0800

    update

--- contracts/Treasury.sol
+++ contracts/Treasury.sol
@@ -1,6 +1,7 @@
 pragma solidity ^0.6.0;
 
 import "@openzeppelin/contracts/math/Math.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
 import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
@@ -16,12 +17,15 @@ import "./owner/Operator.sol";
 import "./utils/Epoch.sol";
 import "./utils/ContractGuard.sol";
 
-contract Treasury is ContractGuard, Epoch {
+contract Treasury is ContractGuard, Epoch,AccessControl{
     using FixedPoint for *;
     using SafeERC20 for IERC20;
     using Address for address;
     using SafeMath for uint256;
     using Safe112 for uint112;
+
+    
+
 
     // ========== FLAGS
     bool public migrated = false;
@@ -37,9 +41,12 @@ contract Treasury is ContractGuard, Epoch {
     uint256 public cashPriceOne;
     uint256 public cashPriceCeiling;
     uint256 public cashPriceFloor;
+    bytes32 public constant EXTENSION = keccak256("EXTENSION");
 
-    // Contractionary Policy
-    mapping(address=>uint256) CPEarned;
+    // Contractionary Policy Earned
+    mapping(address=>uint256) public CPEarned;
+    uint256 public totalBurned ;
+    uint256 public pendingMintedUBC;
 
    
     constructor(
@@ -55,14 +62,15 @@ contract Treasury is ContractGuard, Epoch {
         seigniorageOracle = _seigniorageOracle;
         boardroom = _boardroom;
         ubcusdtPool = _UBCUSDTPool;
-        cashPriceOne = 10**18;
+        cashPriceOne = 10**6;
         cashPriceCeiling = uint256(105).mul(cashPriceOne).div(10**2);
         cashPriceFloor = uint256(95).mul(cashPriceOne).div(10**2);
+        _setupRole(DEFAULT_ADMIN_ROLE,msg.sender);
     }
 
 
     modifier checkMigration {
-        require(!migrated, "Treasury: migrated");
+        require(!migrated, 'Treasury: migrated');
 
         _;
     }
@@ -76,6 +84,24 @@ contract Treasury is ContractGuard, Epoch {
         );
         _;
     }
+
+    function ExtensionMint(uint256 amount) public {
+         require(hasRole(EXTENSION,msg.sender),"Only EXTENSION");
+          IUCashAsset(cash).mint(msg.sender,amount);
+
+    }
+    function ExtensionBurn(uint256 amount) public {
+        require(hasRole(EXTENSION,msg.sender),"Only EXTENSION");
+        IUCashAsset(cash).burnFrom(msg.sender,amount);
+    }
+
+
+    function setParams(uint256 ceiling, uint256 one,uint256 floor) public onlyOperator{
+        cashPriceCeiling = ceiling;
+        cashPriceOne = one;
+        cashPriceFloor = floor;
+    }
+
 
     function getSeigniorageOraclePrice() public view returns (uint256) {
         return _getCashPrice(seigniorageOracle);
@@ -93,46 +119,55 @@ contract Treasury is ContractGuard, Epoch {
         try IOracle(seigniorageOracle).update()  {} catch {}
     }
     
+
     function migrate(address target) public onlyOperator checkOperator {
-        require(!migrated, "Treasury: migrated");
+        require(!migrated, 'Treasury: migrated');
+
         // cash
         Operator(cash).transferOperator(target);
         Operator(cash).transferOwnership(target);
-        IERC20(cash).transfer(target, IERC20(cash).balanceOf(address(this)));
 
+        Operator(boardroom).transferOperator(target);
+        Operator(boardroom).transferOwnership(target);
+    
         // share
         Operator(share).transferOperator(target);
         Operator(share).transferOwnership(target);
-        IERC20(share).transfer(target, IERC20(share).balanceOf(address(this)));
 
         migrated = true;
         emit Migration(target);
     }
 
-    function melter(uint256 amount) public  onlyOneBlock{
+    function melter(uint256 amount) public  onlyOneBlock  checkMigration checkStartTime {
         uint256 cashPrice = _getCashPrice(seigniorageOracle);
-        require(cashPrice <= cashPriceFloor,"Stakeroom not open");
+        require(cashPrice <= cashPriceFloor && cashPrice>0,"melter not open");
         require(amount>0,"can not stake zero");
         IUCashAsset(cash).burnFrom(msg.sender,amount);
-        CPEarned[msg.sender] = CPEarned[msg.sender].add(cashPriceOne.mul(1e18).div(cashPrice).mul(amount).div(1e18));
+        totalBurned = totalBurned.add(amount);
+
+        uint256 pendingAmount = cashPriceOne.mul(1e18).div(cashPrice).mul(amount).div(1e18);
+        pendingMintedUBC = pendingMintedUBC.add(pendingAmount);
+        CPEarned[msg.sender] = CPEarned[msg.sender].add(pendingAmount);
     }
-
-
 
     function redeem() public onlyOneBlock {
         uint256 cashPrice = _getCashPrice(seigniorageOracle);
         require(cashPrice >= cashPriceOne,"price lower than 1");
         require(CPEarned[msg.sender]>0,"earned zero");
-        CPEarned[msg.sender] = 0;
         IUCashAsset(cash).mint(msg.sender,CPEarned[msg.sender]);
+        pendingMintedUBC = pendingMintedUBC.sub(CPEarned[msg.sender]);
+        CPEarned[msg.sender] = 0;
     }
 
-
+    function getTotalPooledUBC() public view returns(uint256){
+        (uint112 poolUBCAmount,,) = IUniswapV2Pair(ubcusdtPool).getReserves();
+        return uint256(poolUBCAmount);
+    }
 
    function allocateSeigniorage()
         external
-        onlyOneBlock
         checkMigration
+        onlyOneBlock
         checkStartTime
         checkEpoch
         checkOperator
@@ -142,26 +177,17 @@ contract Treasury is ContractGuard, Epoch {
         if (cashPrice < cashPriceCeiling) {
            return ;
         }
-        (uint112 poolUBCAmount,,) = IUniswapV2Pair(ubcusdtPool).getReserves();
+        uint256 poolUBCAmount= getTotalPooledUBC();
         uint256 percentage = cashPrice.sub(cashPriceOne);
-        uint256 seigniorage = uint256(poolUBCAmount).mul(percentage).div(1e18);
+        uint256 seigniorage = poolUBCAmount.mul(percentage).div(1e6);
         IUCashAsset(cash).mint(address(this), seigniorage);
         IERC20(cash).safeApprove(boardroom, seigniorage);
         IBoardroom(boardroom).allocateSeigniorage(seigniorage);
         emit BoardroomFunded(now, seigniorage);
     }
 
-
-    event Initialized(address indexed executor, uint256 at);
     event Migration(address indexed target);
     event BoardroomFunded(uint256 timestamp, uint256 seigniorage);
-
-
-
-
-
-
-
 }
 
 
